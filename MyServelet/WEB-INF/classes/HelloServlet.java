1a5d3b11be9baefecb72359c28371137c3922247
HarshaTalatala <harsha.talatala@gmail.com>
2025-11-02 21:02:36 +0530

    MyServlet

--- MyServelet/WEB-INF/classes/HelloServlet.java
+++ MyServelet/WEB-INF/classes/HelloServlet.java
@@ -5,7 +5,8 @@ import jakarta.servlet.http.*;
 public class HelloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException {
-        response.setContentType("text/html");
+            
+        response.setContentType("text/html"); //optional-line
         PrintWriter out = response.getWriter();
         out.println("<h1>Hello from Servlet!</h1>");
     }
